43fbd28776e1c766e0316537b01ff4633aa8eb37
micro1964 <reggy_williams@hotmail.com>
2025-07-14 09:41:19 +0100

    Updated MovieGrid, MovieCard and some images

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import '../styles.css';
 export default function Header() {
   return (
     <header className="header">
-      <img className="logo" src="./CCTN-Logo-Banner_3.png" alt="Cyber Cityz Television Network" />
-      <h2 className="app-subtitle">Educative & entertaining programming across all borders!</h2>
+      <img className="logo" src="./techin9ja_200x200.png" alt="Tech In 9ja" />
+      <h2 className="app-subtitle">Click, learn, succeed!</h2>
     </header>
   );
 }
